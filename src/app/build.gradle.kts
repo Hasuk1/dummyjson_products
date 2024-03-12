0f39019450a7ea4e1d3ed5c009160eaef0f2d313
Perla Brunilda <perlabru@un-m4.kzn.21-school.ru>
2024-03-12 18:08:14 +0300

    Builded apk

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -72,6 +72,14 @@ dependencies {
   kapt(libs.androidx.hilt.compiler)
   implementation(libs.androidx.hilt.navigation.compose)
 
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.junit)
+  androidTestImplementation(libs.androidx.espresso.core)
+  androidTestImplementation(platform(libs.androidx.compose.bom))
+  androidTestImplementation(libs.androidx.ui.test.junit4)
+  debugImplementation(libs.androidx.ui.tooling)
+  debugImplementation(libs.androidx.ui.test.manifest)
+
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.lifecycle.runtime.ktx)
   implementation(libs.androidx.activity.compose)
